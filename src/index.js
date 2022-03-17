821cbfe76c33ffdc303874e6c2304ddc3667aa0c
elsa manang <manangelsa07@gmail.com>
2022-03-17 15:39:43 +0100

    libraries call

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, {Component} from 'react';
+import styles from './styles.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import PropTypes from 'prop-types';
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
