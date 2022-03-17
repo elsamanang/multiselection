2c744824fc7699e2188207cf8245b6b7d6621855
elsa manang <manangelsa07@gmail.com>
2022-03-17 16:45:09 +0100

    Updates

---
+++ precache-manifest.47a25850bc3ee4bba780113d3e17eec3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fd3f731be0de7d3a8bd0011bd670fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed100ead9930848b0e70",
+    "url": "./static/css/2.4f26e51f.chunk.css"
+  },
+  {
+    "revision": "9317ac8a961c6b0dc381",
+    "url": "./static/css/main.a5680edc.chunk.css"
+  },
+  {
+    "revision": "ed100ead9930848b0e70",
+    "url": "./static/js/2.e64a82de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e64a82de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9317ac8a961c6b0dc381",
+    "url": "./static/js/main.6eaf5ee5.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.6eaf5ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17bf55049786fe172a5",
+    "url": "./static/js/runtime-main.7d22af96.js"
+  }
+]);
